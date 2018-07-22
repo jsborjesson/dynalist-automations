56d443163e150f15be550964ba8cf904d4bd458b
Jimmy Börjesson <alcesleo@gmail.com>
2018-07-22 19:00:49 +0200

    Add support for fetching documents

--- lib/dynalist.rb
+++ lib/dynalist.rb
@@ -1,18 +1,27 @@
+require "http"
+
 class Dynalist
   def initialize
     @api_base = ENV.fetch("DYNALIST_API_BASE")
     @api_token = ENV.fetch("DYNALIST_API_TOKEN")
   end
 
-  def documents
+  def files
     make_request("file/list")
+  end
+
+  def document(file_id)
+    make_request("doc/read", file_id: file_id)
   end
 
   private
 
-  def make_request(endpoint)
+  def make_request(endpoint, options = {})
     url = [@api_base, endpoint].join("/")
-    response = HTTP.post(url, json: { token: @api_token})
+    body = options.merge(token: @api_token)
+
+    response = HTTP.post(url, json: body)
+
     JSON.parse(response)
   end
 end
--- test/dynalist_test.rb
+++ test/dynalist_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 require "dynalist"
 
 class DynalistTest < Minitest::Test
-  def response
+  def files_response
     {
       "_code"=>"Ok",
       "root_file_id"=>"SK4EYgx-6Ovm7ODXCVHJLPLN",
@@ -10,13 +10,13 @@ class DynalistTest < Minitest::Test
       [
         {
           "id"=>"DHm5grx7XCauWaMfhw9Wqd-K",
-          "title"=>"Document 1",
+          "title"=>"file 1",
           "type"=>"document",
           "permission"=>4
         },
         {
           "id"=>"i04ycIs7a2b2mbVBRo2ugvRH",
-          "title"=>"Document 2",
+          "title"=>"file 2",
           "type"=>"document",
           "permission"=>4
         },
@@ -24,16 +24,51 @@ class DynalistTest < Minitest::Test
     }
   end
 
-  def setup
+  def document_response
+    {"_code"=>"Ok",
+     "title"=>"Automations Test",
+     "nodes"=>
+    [{"id"=>"root",
+      "content"=>"Automations Test",
+      "note"=>"",
+      "checked"=>false,
+      "children"=>["XsONM0ZqP1zm8zYvaNeOGGFc", "xbexUA6PK1ZJ7yHk1UMPbCcv"]},
+    {"id"=>"XsONM0ZqP1zm8zYvaNeOGGFc",
+     "content"=>"One",
+     "note"=>"",
+     "checked"=>false},
+     {"id"=>"Qp5qIiccr1XuAP6rJL5RX_jt",
+      "content"=>"a",
+      "note"=>"",
+      "checked"=>false},
+      {"id"=>"uT84b0GMNQA2tEF60EQZJ267",
+       "content"=>"b",
+       "note"=>"",
+       "checked"=>false},
+       {"id"=>"xbexUA6PK1ZJ7yHk1UMPbCcv",
+        "content"=>"Two",
+        "note"=>"",
+        "checked"=>false,
+        "children"=>["Qp5qIiccr1XuAP6rJL5RX_jt", "uT84b0GMNQA2tEF60EQZJ267"]}]}
   end
 
-  def test_documents
+  def setup
+    @api = Dynalist.new
+  end
+
+  def test_files
     stub_request(:post, "https://dynalist.io/api/v1/file/list")
       .with(body: { token: "abcd1234" }.to_json)
-      .to_return(body: response.to_json)
+      .to_return(body: files_response.to_json)
 
-    api = Dynalist.new
+    assert_equal files_response, @api.files
+  end
 
-    assert_equal response, api.documents
+  def test_document
+    stub_request(:post, "https://dynalist.io/api/v1/doc/read")
+      .with(body: { file_id: "file_id", token: "abcd1234" }.to_json)
+      .to_return(body: document_response.to_json)
+
+    assert_equal document_response, @api.document("file_id")
   end
 end
