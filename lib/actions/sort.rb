65ce5b9cb227759d85cb993d0c4dd3f191322af8
Jimmy Börjesson <alcesleo@gmail.com>
2018-09-18 00:18:27 +0200

    Sort by date asc

--- lib/actions/sort.rb
+++ lib/actions/sort.rb
@@ -14,6 +14,7 @@ class Sort
         .children(parent)
         .reject { |b| b.date.nil? } # Do not reorder bullets without a date
         .sort_by(&:date)
+        .reverse
         .map { |b|
           # Always setting zero gives us a reverse sort without keeping track of an index
           MoveBullet.new(node_id: b.id, parent_id: parent.id, index: 0)
